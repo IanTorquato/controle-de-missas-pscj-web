95ae9faf162a01c6247aed116c79d20b4125e223
Ian da Conceição da Silva <iantorquato2@gmail.com>
2020-08-21 19:07:31 -0300

    Detalhes no designer da página de contatos

--- src/pages/Contato/index.tsx
+++ src/pages/Contato/index.tsx
@@ -12,8 +12,7 @@ const Contato = () => {
 				<section className="pessoas">
 					<img src={programador} alt="Foto do desenvolvedor" className="imgContato" />
 
-					<div className="dadosPessoa">
-
+					<div>
 						<div>
 							<h1 className="nomeContato">Ian da Conceição da Silva</h1>
 							<h2 className="setorContato">Programador</h2>
@@ -22,44 +21,48 @@ const Contato = () => {
 						<ul className="redesSociaisContato">
 							<div>
 								<li>
-									<span className="spanGmail"><FaEnvelope size={30} /></span>
-
-									<a href="http://gmail.com" target="_blank" rel="noopener noreferrer">Gmail</a>
+									<a href="http://gmail.com" target="_blank" rel="noopener noreferrer">
+										<span className="spanGmail"> <FaEnvelope size={30} /> </span>
+										Gmail
+									</a>
 								</li>
 
 								<li>
-									<span className="spanFace"><FaFacebook size={30} /></span>
-
-									<a href="https://www.facebook.com/ian.conceicaodasilva" target="_blank"
-										rel="noopener noreferrer">Facebook</a>
+									<a href="https://www.facebook.com/ian.conceicaodasilva" target="_blank" rel="noopener noreferrer">
+										<span className="spanFace"> <FaFacebook size={30} /> </span>
+										Facebook
+									</a>
 								</li>
 
 								<li>
-									<span className="spanInsta"><FaInstagram size={30} /></span>
-
-									<a href="https://www.instagram.com/ian_1408" target="_blank" rel="noopener noreferrer">Instagram</a>
+									<a href="https://api.whatsapp.com/send?phone=5548998224086" target="_blank" rel="noopener noreferrer">
+										<span className="spanWhats"> <FaWhatsapp size={30} /> </span>
+										Whatsapp
+									</a>
 								</li>
 							</div>
 
 							<div>
 								<li>
-									<span className="spanWhats"><FaWhatsapp size={30} /></span>
-
-									<a href="https://api.whatsapp.com/send?phone=5548998224086" target="_blank"
-										rel="noopener noreferrer">Whatsapp</a>
+									<a href="https://www.instagram.com/ian_1408" target="_blank" rel="noopener noreferrer">
+										<span className="spanInsta"> <FaInstagram size={30} /> </span>
+										Instagram
+									</a>
 								</li>
 
 								<li>
-									<span className="spanLinkedin"><FaLinkedin size={30} /></span>
-
 									<a href="https://www.linkedin.com/in/ian-da-concei%C3%A7%C3%A3o-da-silva-67549b1a2/" target="_blank"
-										rel="noopener noreferrer">Linkedin</a>
+										rel="noopener noreferrer">
+										<span className="spanLinkedin"> <FaLinkedin size={30} /> </span>
+										Linkedin
+									</a>
 								</li>
 
 								<li>
-									<span className="spanGithub"><FaGithub size={30} /></span>
-
-									<a href="https://github.com/IanTorquato" target="_blank" rel="noopener noreferrer">GitHub</a>
+									<a href="https://github.com/IanTorquato" target="_blank" rel="noopener noreferrer">
+										<span className="spanGithub"> <FaGithub size={30} /> </span>
+										GitHub
+									</a>
 								</li>
 							</div>
 						</ul>
@@ -69,7 +72,7 @@ const Contato = () => {
 				<section className="pessoas">
 					<img src={designer} alt="Foto da designer" className="imgContato" />
 
-					<div className="dadosPessoa">
+					<div>
 						<div>
 							<h1 className="nomeContato">Ana Clara Vargas Rodrigues</h1>
 							<h2 className="setorContato">Designer</h2>
@@ -79,31 +82,35 @@ const Contato = () => {
 						<ul className="redesSociaisContato">
 							<div>
 								<li>
-									<span className="spanGmail"><FaEnvelope size={30} /></span>
-
-									<a href="http://gmail.com" target="_blank" rel="noopener noreferrer">Gmail</a>
+									<a href="http://gmail.com" target="_blank" rel="noopener noreferrer">
+										<span className="spanGmail"> <FaEnvelope size={30} /> </span>
+										Gmail
+									</a>
 								</li>
 
 								<li>
-									<span className="spanFace"><FaFacebook size={30} /></span>
-
 									<a href="https://pt-br.facebook.com/anaclara.vargasrodrigues" target="_blank"
-										rel="noopener noreferrer">Facebook</a>
+										rel="noopener noreferrer">
+										<span className="spanFace"> <FaFacebook size={30} /> </span>
+										Facebook
+									</a>
 								</li>
 							</div>
 
 							<div>
 								<li>
-									<span className="spanInsta"><FaInstagram size={30} /></span>
-
-									<a href="https://www.instagram.com/anaclaravargs" target="_blank" rel="noopener noreferrer">Instagram</a>
+									<a href="https://www.instagram.com/anaclaravargs" target="_blank" rel="noopener noreferrer">
+										<span className="spanInsta"> <FaInstagram size={30} /> </span>
+										Instagram
+									</a>
 								</li>
 
 								<li>
-									<span className="spanWhats"><FaWhatsapp size={30} /></span>
-
 									<a href="https://api.whatsapp.com/send?phone=5548998301146" target="_blank"
-										rel="noopener noreferrer">Whatsapp</a>
+										rel="noopener noreferrer">
+										<span className="spanWhats"> <FaWhatsapp size={30} /> </span>
+										Whatsapp
+									</a>
 								</li>
 							</div>
 						</ul>
