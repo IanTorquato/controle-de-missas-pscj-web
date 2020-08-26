696a2a6b1b02a1a83a9aea6bf3a2bdc285b02125
Ian da Conceição da Silva <iantorquato2@gmail.com>
2020-08-26 20:15:59 -0300

    Começando o novo design da página Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,17 @@
 import React, { useState, useEffect, useContext } from 'react'
+import { BiChurch } from 'react-icons/bi'
+import { GiHealthNormal } from 'react-icons/gi'
+import { RiComputerLine } from 'react-icons/ri'
 import { Link } from 'react-router-dom'
 
-import './styles.css'
-import logo from '../../assets/logo.webp'
+// import logo from '../../assets/logo.webp'
+// 					<img className="logo" src={logo} alt="Brasão da Paróquia" />
+import igrejaCentro from '../../assets/dentroIgrejaCentro.webp'
 import Footer from '../../components/Footer'
 import api from '../../services/api'
 import LoginContext from '../../contexts/login'
+
+import './styles.css'
 
 interface Missa {
 	id: number
@@ -39,55 +45,63 @@ const Home = () => {
 
 	return (
 		<>
-			<div className="imgFundo" onDoubleClick={deslogar}>
-				<section className="secHome">
-					<div className="centralInfo" onClick={scrollCronograma}>
-						<img className="logo" src={logo} alt="Brasão da Paróquia" />
-						<hr className="linhaHome" />
+			<section className="secHome" onDoubleClick={deslogar}>
+				<h1 className="tituloSSCJ">Santuário Sagrado <br /> Coração de <br /> Jesus</h1>
 
-						<h1 className="tituloSCJ">Santuário Sagrado <br /> Coração de Jesus</h1>
-
-						<hr className="linhaHome" />
-						<h2 className="subTituloSCJ">Gravatal - SC</h2>
+				<div className="alinhaHorizontal">
+					<div className="alinhaHorizontal">
+						<div className="curiosidade" id="corVermelha" >
+							<BiChurch size={64} />
+						</div>
+						<div className="curiosidade" id="corDourada" >
+							<GiHealthNormal size={64} />
+						</div>
+						<div className="curiosidade" id="corAzul" >
+							<RiComputerLine size={64} />
+						</div>
 					</div>
-				</section>
 
-				<section className="sectionBotoes">
-					<div className="alinhaCentro">
-						<Link to="/cadastrar-missa" className="btnCadastrar" onClick={scrollTopo}>Cadastrar Missa</Link>
-						<Link to="/editar-missa" className="btnEditar" onClick={scrollTopo}>Editar Missa</Link>
+					<div className="imagemDecorada">
+						<img src={igrejaCentro} alt="Igreja do Centro" />
 					</div>
-				</section>
+				</div>
+			</section>
 
-				<section className="cronograma">
-					<h1 className="proximasMissas">PRÓXIMAS MISSAS</h1>
+			<section className="sectionBotoes">
+				<div className="alinhaCentro">
+					<Link to="/cadastrar-missa" className="btnCadastrar" onClick={scrollTopo}>Cadastrar Missa</Link>
+					<Link to="/editar-missa" className="btnEditar" onClick={scrollTopo}>Editar Missa</Link>
+				</div>
+			</section>
 
-					<h2>{erroMissas}</h2>
+			<section className="cronograma">
+				<h1 className="proximasMissas">PRÓXIMAS MISSAS</h1>
 
-					<div className="gridMissas">
-						{missas.map(missa => {
+				<h2>{erroMissas}</h2>
 
-							const dataCortada = missa.data.split('/')
-							const dataInvertida = `${dataCortada[2]}/${dataCortada[1]}/${dataCortada[0]}`
+				<div className="gridMissas">
+					{missas.map(missa => {
 
-							const diaMissa = new Date(Date.parse(`${dataInvertida}`))
+						const dataCortada = missa.data.split('/')
+						const dataInvertida = `${dataCortada[2]}/${dataCortada[1]}/${dataCortada[0]}`
 
-							const diasSemana = ['DOMINGO', 'SEGUNDA-FEIRA', 'TERÇA-FEIRA', 'QUARTA-FEIRA',
-								'QUINTA-FEIRA', 'SEXTA-FEIRA', 'SÁBADO']
+						const diaMissa = new Date(Date.parse(`${dataInvertida}`))
 
-							return (
-								<div key={missa.id} className="detalhesMissa">
-									<h1 className="tituloMissa">{missa.data.slice(0, 5)} - {missa.hora}</h1>
+						const diasSemana = ['DOMINGO', 'SEGUNDA-FEIRA', 'TERÇA-FEIRA', 'QUARTA-FEIRA',
+							'QUINTA-FEIRA', 'SEXTA-FEIRA', 'SÁBADO']
 
-									<h2 className="subTituloMissa">
-										{diasSemana[diaMissa.getDay()]} | {missa.local_id === 1 ? 'CENTRO' : 'TERMAS'}
-									</h2>
-								</div>
-							)
-						})}
-					</div>
-				</section>
-			</div>
+						return (
+							<div key={missa.id} className="detalhesMissa">
+								<h1 className="tituloMissa">{missa.data.slice(0, 5)} - {missa.hora}</h1>
+
+								<h2 className="subTituloMissa">
+									{diasSemana[diaMissa.getDay()]} | {missa.local_id === 1 ? 'CENTRO' : 'TERMAS'}
+								</h2>
+							</div>
+						)
+					})}
+				</div>
+			</section>
 			<Footer />
 		</>
 	)
