5692df5e3fe8a41f0ab901a5ad65b73e82d59e5c
Ian da Conceição da Silva <iantorquato2@gmail.com>
2020-10-27 17:49:50 -0300

    Integrando página de Detalhes com a api

--- src/pages/DetalhesMissa/index.tsx
+++ src/pages/DetalhesMissa/index.tsx
@@ -6,31 +6,42 @@ import { FaMapMarkedAlt } from 'react-icons/fa'
 import { HiUserGroup } from 'react-icons/hi'
 
 import api from '../../services/api'
+import Header from '../../components/Header'
 import Missa from '../../utils/interfaces'
-
-import imgTemporaria from '../../assets/dentroIgrejaCentro.svg'
+import { formatDiaMesHora } from '../../utils/tratandoDatas'
 
 import './styles.css'
+
+interface Usuarios {
+	id: number
+	nome: string
+	foto: string
+	quantidade_pessoas: number
+}
 
 interface RouteParams {
 	id: string
 }
 
 const DetalhesMissa: React.FC = () => {
-	// const [missa, setMissa] = useState<Missa | null>(null)
+	const [missa, setMissa] = useState<Missa | null>(null)
+	const [usuarios, setUsuarios] = useState<Usuarios[] | null>(null)
 
 	const { goBack } = useHistory()
 
 	const { id } = useParams<RouteParams>()
 
-	// useEffect(() => {
-	// 	api.get(`missas?missa_id=${id}`)
-	// 		.then(({ data }) => setMissa(data))
-	// 		.catch(({ response }) => {
-	// 			console.log(response)
-	// 			alert(response.data.erro || 'Falha ao listar uma única missa.')
-	// 		})
-	// }, [])
+	useEffect(() => {
+		api.get(`missas?missa_id_usuarios=${id}`)
+			.then(({ data }) => {
+				setMissa(formatDiaMesHora(data.missa).missaSerializada)
+				if (data.usuarios) { setUsuarios(data.usuarios) }
+			})
+			.catch(({ response }) => {
+				console.log(response)
+				alert(response.data.erro || 'Falha ao listar uma única missa.')
+			})
+	}, [])
 
 	function excluirMissa(id: number) {
 		// eslint-disable-next-line no-restricted-globals
@@ -47,68 +58,85 @@ const DetalhesMissa: React.FC = () => {
 		}
 	}
 
+	const nomeLocal = missa?.local_id === 1 ? 'Centro' : 'Termas'
+	const urlImagem = `${process.env.REACT_APP_URL_BANCO}/uploads/fotosLocais/igreja${nomeLocal}.png`
+
 	return (
-		<section className="secDetalhesMissa">
-			<header>
-				<img src={imgTemporaria} alt="Igreja" />
+		<>
+			<Header />
 
-				<div className="dadosMissa">
-					<div>
-						<h1>Solenidade de N. Srª. Aparecida</h1>
+			<section className="secDetalhesMissa">
+				{missa && (
+					<>
+						<header>
+							<img src={urlImagem} alt="Igreja" />
 
-						<div>
-							<div onClick={() => excluirMissa(+id)}>
-								<BiTrash size={24} color="#e5e5e5" />
+							<div className="dadosMissa">
+								<div>
+									<h1>{missa.nome}</h1>
+
+									<div>
+										<div onClick={() => excluirMissa(+id)}>
+											<BiTrash size={24} color="#e5e5e5" />
+										</div>
+
+										<Link to={`/editar-missa/${id}`}>
+											<BiEditAlt size={24} color="#e5e5e5" />
+										</Link>
+									</div>
+								</div>
+
+								<div>
+									<span>
+										<BiCalendar size={24} color="#e5e5e5" />
+										{missa.data}
+									</span>
+
+									<span>
+										<FiClock size={24} color="#e5e5e5" />
+										{missa.hora}
+									</span>
+
+									<span>
+										<FaMapMarkedAlt size={24} color="#e5e5e5" />
+										{missa.local_id === 1 ? 'Centro' : 'Termas'}
+									</span>
+
+									<span>
+										<HiUserGroup size={24} color="#e5e5e5" />
+										{missa.pessoas_cadastradas}/{missa.max_pessoas}
+									</span>
+								</div>
 							</div>
+						</header>
 
-							<Link to={`/editar-missa/${id}`}>
-								<BiEditAlt size={24} color="#e5e5e5" />
-							</Link>
-						</div>
-					</div>
+						<hr />
 
-					<div>
-						<span>
-							<BiCalendar size={24} color="#e5e5e5" />
-							12/10
-						</span>
+						{usuarios ?
+							<div className="gridUsuarios">
+								{usuarios.map(usuario => (
+									<div className="usuario" key={usuario.id}>
+										<img src={usuario.foto} alt="Avatar do Usuário" />
 
-						<span>
-							<FiClock size={24} color="#e5e5e5" />
-							19:00
-						</span>
-
-						<span>
-							<FaMapMarkedAlt size={24} color="#e5e5e5" />
-							Centro
-						</span>
-
-						<span>
-							<HiUserGroup size={24} color="#e5e5e5" />
-							94/100
-						</span>
-
-					</div>
-				</div>
-			</header>
-
-			<hr />
-
-			<div className="gridUsuarios">
-				<div className="usuario">
-					<img src={imgTemporaria} alt="Avatar do Usuário" />
-
-					<div>
-						<span className="nomeUsuario">Ian da Conceição da SIlva</span>
-						<br />
-						<span className="quantUsuario">
-							<HiUserGroup size={24} color="#e5e5e5" />
-							16/100
-						</span>
-					</div>
-				</div>
-			</div>
-		</section>
+										<div>
+											<span className="nomeUsuario">{usuario.nome}</span>
+											<br />
+											<span className="quantUsuario">
+												<HiUserGroup size={24} color="#e5e5e5" />
+												{usuario.quantidade_pessoas}/{missa.max_pessoas}
+											</span>
+										</div>
+									</div>
+								))}
+							</div> :
+							<div>
+								<h3>Nenhum usuário cadastrado</h3>
+							</div>
+						}
+					</>
+				)}
+			</section>
+		</>
 	)
 }
 
