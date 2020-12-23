b398b1a9fc999c3569e20d5cdefc51c79bc0aa52
Ian Torquato <iantorquato2@gmail.com>
2020-12-23 19:28:30 -0300

    Botão de voltar para Home

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 import pageNotFound from '../../assets/pageNotFound.svg'
 
@@ -9,7 +10,10 @@ const NotFound: React.FC = () => {
 		<section className="secNotFound">
 			<h1>Algo de errado não está certo...</h1>
 
-			<div>Hmm... Acho que você veio parar na dimensão errada. Que tal voltar ao ponto de partida?</div>
+			<div>
+				Hmm... Acho que você veio parar na dimensão errada.
+				Que tal voltar ao ponto de partida? É só <Link to="/">Clicar Aqui</Link>.
+			</div>
 
 			<img src={pageNotFound} alt="Página não encontrada" />
 		</section>
