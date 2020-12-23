89738b2e558776195e031de5bbe42b6e8c28fce2
Ian Torquato <iantorquato2@gmail.com>
2020-12-23 18:14:24 -0300

    Ajustar cadastro/edição de missas

--- src/components/FormMissas/index.tsx
+++ src/components/FormMissas/index.tsx
@@ -1,10 +1,11 @@
 import React, { useState, FormEvent } from 'react'
+import { useHistory } from 'react-router-dom'
 import { format } from 'date-fns'
 import { BiChurch } from 'react-icons/bi'
 import { FaMapMarkedAlt } from 'react-icons/fa'
 import { HiUserGroup } from 'react-icons/hi'
 
-// import api from '../../services/api'
+import api from '../../services/api'
 import Missa from '../../utils/interfaces'
 import flechaTorta from '../../assets/icons/flechaTorta.svg'
 import sublinhado from '../../assets/sublinhado.png'
@@ -25,6 +26,8 @@ const FormMissas: React.FC<FormMissa> = ({ titulo, txtBtn, missa, mensagemEsquer
 	const [max_pessoas, setMax_pessoas] = useState(missa?.max_pessoas || '')
 	const [data_hora, setData_hora] = useState(missa?.data_hora ? ordenaData(missa.data_hora.split('T')) : '')
 
+	const { push } = useHistory()
+
 	function ordenaData([data, hora]: string[]) {
 		const [dia, mes, ano] = data.split('/')
 
@@ -36,31 +39,32 @@ const FormMissas: React.FC<FormMissa> = ({ titulo, txtBtn, missa, mensagemEsquer
 
 		if (data_hora) {
 			const dadosMissa = { nome, local_id, data_hora, max_pessoas }
-			console.log(dadosMissa)
-			// if (!missa) {
-			// 	api.post('missas', dadosMissa)
-			// 		.then(({ data }) => {
-			// 			alert(data.mensagem)
-			// 		})
-			// 		.catch(({ response }) => {
-			// 			console.log(response)
-			// 			return alert(response.data.erro || 'Falha ao cadastrar a missa')
-			// 		})
-			// } else {
-			// 	api.put(`missas/${missa.id}`, dadosMissa)
-			// 		.then(({ data }) => {
-			// 			alert(data.mensagem)
-			// 		})
-			// 		.catch(({ response }) => {
-			// 			console.log(response)
-			// 			return alert(response.data.erro || 'Falha ao atualizar a missa')
-			// 		})
-			// }
 
-			setNome('')
-			setLocal_id(0)
-			setMax_pessoas('')
-			setData_hora('')
+			if (!missa) {
+				api.post('missas', dadosMissa)
+					.then(({ data }) => {
+						setNome('')
+						setLocal_id(0)
+						setMax_pessoas('')
+						setData_hora('')
+
+						alert(data.mensagem)
+					})
+					.catch(({ response }) => {
+						console.log(response)
+						alert(response.data.erro || 'Falha ao cadastrar a missa')
+					})
+			} else {
+				api.put(`missas/${missa.id}`, dadosMissa)
+					.then(({ data }) => {
+						alert(data.mensagem)
+						push('/lista-missas')
+					})
+					.catch(({ response }) => {
+						console.log(response)
+						alert(response.data.erro || 'Falha ao atualizar a missa')
+					})
+			}
 		}
 	}
 
@@ -75,7 +79,7 @@ const FormMissas: React.FC<FormMissa> = ({ titulo, txtBtn, missa, mensagemEsquer
 
 				<div className="containerInputsForm">
 					<div>
-						<input type="text" name="nome" className="nome" placeholder="Dê um nome à missa" required
+						<input type="text" className="nome" placeholder="Dê um nome à missa" required
 							onChange={({ target }) => setNome(target.value)} value={nome} />
 
 						<BiChurch size={20} fill="#747474" />
@@ -83,12 +87,12 @@ const FormMissas: React.FC<FormMissa> = ({ titulo, txtBtn, missa, mensagemEsquer
 
 					<div>
 						<input className="dataHora" type="datetime-local" onChange={({ target }) => setData_hora(target.value)} required
-							min={format(new Date(), "yyyy-MM-dd'T'HH:mm")} name="dataHora" value={data_hora} />
+							min={format(new Date(), "yyyy-MM-dd'T'HH:mm")} value={data_hora} />
 					</div>
 
 					<div>
 						<div>
-							<select name="local" value={local_id} onChange={({ target }) => setLocal_id(+target.value)} required>
+							<select value={local_id} onChange={({ target }) => setLocal_id(+target.value)} required>
 								<option value="" hidden>Selecione um local</option>
 								<option value="1">Centro</option>
 								<option value="2">Termas</option>
@@ -98,7 +102,7 @@ const FormMissas: React.FC<FormMissa> = ({ titulo, txtBtn, missa, mensagemEsquer
 						</div>
 
 						<div>
-							<input type="number" name="maxPessoas" className="maxPessoas" placeholder="Máximo de pessoas" min={1} required
+							<input type="number" className="maxPessoas" placeholder="Máximo de pessoas" min={1} required
 								onChange={({ target }) => setMax_pessoas(+target.value)} value={max_pessoas} />
 
 							<HiUserGroup size={20} fill="#747474" />
